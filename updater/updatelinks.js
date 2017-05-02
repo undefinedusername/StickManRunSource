64ec5ec8e3fa216e59993257a08258e4c1846421
undefinedusername <noah.enger@weatherfordisd.net>
2017-05-02 13:17:57 -0500

    Create updatelinks.js

---
+++ updater/updatelinks.js
@@ -0,0 +1,21 @@
+//  Checks these links
+//  default is github
+//  Made by Noah J. Enger, 5-2-17
+//  contact me before editing at noahenger@gmail.com
+
+//links
+var updatelinks={
+  github:" "
+}
+
+//check updates
+checkforupdates(){
+
+}
+
+//openlink
+openthroughgithub(){
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
