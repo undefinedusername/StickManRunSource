c2c350f4e32bdc4f4fda91ba8ee336b0482c656c
undefinedusername <noah.enger@weatherfordisd.net>
2017-05-02 13:29:57 -0500

    Create sprite-animation-demo.js

---
+++ package.nw/stuff/sprite-animation-demo.js
@@ -0,0 +1,140 @@
+// Copyright 2013 William Malone (www.williammalone.com)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+ 
+(function() {
+	// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+	// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+	// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+	// MIT license
+
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+(function () {
+			
+	var coin,coinImage,iconImage,icon;					
+
+	function gameLoop () {
+	
+	  window.requestAnimationFrame(gameLoop);
+   icon.update();
+   icon.render();
+	  coin.update();
+	  coin.render();
+	}
+	
+	function sprite (options) {
+	
+		var that = {},
+			frameIndex = 0,
+			tickCount = 0,
+			ticksPerFrame = options.ticksPerFrame || 0,
+			numberOfFrames = options.numberOfFrames || 1;
+		
+		that.context = options.context;
+		that.width = options.width;
+		that.height = options.height;
+		that.image = options.image;
+		
+		that.update = function () {
+
+            tickCount += 1;
+
+            if (tickCount > ticksPerFrame) {
+
+				tickCount = 0;
+				
+                // If the current frame index is in range
+                if (frameIndex < numberOfFrames - 1) {	
+                    // Go to the next frame
+                    frameIndex += 1;
+                } else {
+                    frameIndex = 0;
+                }
+            }
+        };
+		
+		that.render = function () {
+		
+		  // Clear the canvas
+		  that.context.clearRect(0, 0, that.width, that.height);
+		  
+		  // Draw the animation
+		  that.context.drawImage(
+		    that.image,
+		    frameIndex * that.width / numberOfFrames,
+		    0,
+		    that.width / numberOfFrames,
+		    that.height,
+		    0,
+		    0,
+		    that.width / numberOfFrames,
+		    that.height);
+		};
+		
+		return that;
+	}
+	
+	// Create sprite sheet
+	coinImage = new Image();	
+	iconImage = new Image();
+
+	// Create sprite
+	coin = sprite({
+		context: canvas.getContext("2d"),
+		width: 120,
+		height: 22,
+		image: coinImage,
+		numberOfFrames: 5,
+		ticksPerFrame: 10
+	});
+	
+icon = sprite({
+context: canvas.getContext("2d"),
+		width: 576,
+		height: 576,
+		image: iconImage,
+		numberOfFrames: 324,
+		ticksPerFrame: 1
+});
+
+	// Load sprite sheet
+	coinImage.addEventListener("load", gameLoop);
+	coinImage.src = "stuff/images/winsprite.png";
+
+iconImage.addEventListener("load", gameLoop);
+	iconImage.src = "stuff/images/iconsprites.png";
+} ());
+
